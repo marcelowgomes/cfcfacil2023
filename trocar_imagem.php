a0b8c2fdd27af0b4a5afa34f5b9ecffe9aa2e052
Marcelo Gomes <marcelo@tagscriativa.com.br>
2023-05-11 16:48:15 -0300

    121

--- trocar_imagem.php
+++ trocar_imagem.php
@@ -1,4 +1,17 @@
+
+
 <?php
+
+if (!empty($_SESSION['id_user'])) {
+} else {
+	$_SESSION['msg'] = "Área restrita";
+	header("Location: login.php");
+}
+
+include_once 'bd/conexao.php';
+
+
+
 if(isset($_FILES['imagem_perfil'])) {
     $file_name = $_FILES['imagem_perfil']['name'];
     $file_tmp = $_FILES['imagem_perfil']['tmp_name'];
@@ -10,14 +23,21 @@ if(isset($_FILES['imagem_perfil'])) {
     if($n_imagens == 0) {
     $sql = "INSERT INTO imagens_perfil (caminho_imagem, id_usuario) VALUES ('$file_destination', '$id')";
     if (mysqli_query($conn, $sql)) {
-        echo("<h1>Imagem de Perfil adicionada com sucesso!</h1>");
+        echo("<h1>Imagem de Perfil adicionada com sucesso 2!</h1>");
     }else{
         echo("<h1>Houve um erro na adição de Imagem de Perfil</h1>");
     }
 }else{
     $sql = "UPDATE imagens_perfil SET caminho_imagem = '$file_destination' WHERE id_usuario = $id";
     if (mysqli_query($conn, $sql)) {
-        echo("<h1>Imagem de Perfil atualizada com sucesso!</h1>");
+        ?>
+<script>
+ alert("Foto Adicionada com sucesso");
+
+ window.location.href = "perfil_aluno/<?php echo $id ?>";
+</script>
+
+<?php
     }else{
         echo("<h1>Houve um erro na atualização de Imagem de Perfil</h1>");
     }
